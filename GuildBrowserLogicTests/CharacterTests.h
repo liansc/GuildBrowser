67f121d612f421b7ad1d4b2a1e73f140ab1d5f22
liansc <lianshuaichao@gmail.com>
2012-12-29 15:09:22 +0800

    n

---
+++ GuildBrowserLogicTests/CharacterTests.h
@@ -0,0 +1,13 @@
+//
+//  CharacterTests.h
+//  GuildBrowser
+//
+//  Created by sky on 12-12-29.
+//  Copyright (c) 2012年 Charlie Fulton. All rights reserved.
+//
+
+#import <SenTestingKit/SenTestingKit.h>
+
+@interface CharacterTests : SenTestCase
+
+@end
